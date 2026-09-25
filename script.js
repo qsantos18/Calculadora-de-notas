0487c2c2f1ba64c05cc73d282c0f67e57e960cb9
qsantos18 <q.almeida@escola.pr.gov.br>
2026-09-25 13:55:11 +0000

    ..

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function calcularMedia()
+function calcularMedia(){
     const textoNotal = document.getElementById("nota1"). value;
     const textoNota2 = document.getElementById("nota2"). value;
     const textoNota3 = document.getElementById("nota3"). value;
+
+    if (textoNota1 === "" || textoNota2 === "" || textoNota3 === ""){
+        alert("ATENÇAO! preencha todas as notas.")
+        return;
+    }
 
 const nota1 = Number(textoNotal);
 const nota2 = Number(textoNota2);
@@ -13,7 +18,14 @@ let situacao;
 
 if (media >= 7){
     situacao = "APROVADO";
-}
-if (media >= 5){
+} else if (media >=5){
+    situacao = "RECUPERAÇAO";
+} else {
     situacao = "REPROVADO";
+} 
+const mediaFormatada = media.toFixed(1);
+
+Content.getElementById("resultado").textContent =
+                    "Media: "+ mediaFormatada+ " - Situação: "+ situacao;
+
 }
